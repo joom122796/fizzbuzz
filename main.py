c9b95c1f833234b4238601c0cb683e5322fc32f2
joom <113173627+joom122796@users.noreply.github.com>
2022-11-17 09:48:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 number = int(input('Enter a number: '))
 base1 = int(input('Enter the first base no. you would like to trigger the phrase Fizz: '))
-base2 = int(input('Enter the second base no. you would like to trigger the phrase Fizz: '))
+base2 = int(input('Enter the second base no. you would like to trigger the phrase Buzz: '))
 
-if number % base1 == 0:
-    print('fizz')
-if number % base2 == 0:
-    print('buzz')
 if number % base1 == 0 and number % base2 == 0:
     print('fizzbuzz')
+elif number % base1 == 0:
+    print('fizz')
+elif number % base2 == 0:
+    print('buzz')
+
+    
 if number > 1:
    for i in range(2,number):
        if (number % i) != 0:
